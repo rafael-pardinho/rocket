7b6a175ad3017b02b9e60abd8f7cfed410593a78
Rafael Pardinho <rafael.pardinho@rocketclouds.com.br>
2023-12-07 15:31:44 -0300

    feat: integrando service course ao modelo course.entity

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Course } from './courses.entity';
 
 @Injectable()
-export class CoursesService {}
+export class CoursesService {
+    private courses: Course[] = [
+        {
+            id: 1,
+            name: 'Cardio',
+            description: 'Curso de Cardio',
+            tags: ['Cardio', 'Categoria'],
+        },
+    ]
+}
