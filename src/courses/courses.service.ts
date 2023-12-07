835960cb95e6363cb9e215f7e85be145e4c585a8
Rafael Pardinho <rafael.pardinho@rocketclouds.com.br>
2023-12-07 16:50:17 -0300

    feat: tratamento de erro no metoro findone

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Course } from './courses.entity';
 
 @Injectable()
@@ -16,8 +16,12 @@ export class CoursesService {
         return this.courses;
     }
 
-    findOne(id: number){
-        return this.courses.find(course => course.id === id)
+    findOne(id: number) {
+        const course = this.courses.find(course => course.id === id)
+        if(!course) {
+            throw new NotFoundException(`Course ID ${id} not found`)
+        }
+        return course
 
     }
 
@@ -27,7 +31,7 @@ export class CoursesService {
 
     update(id: number, updateCourseDTO: any) {
         const existCourse = this.findOne(id)
-        if(existCourse){
+        if (existCourse) {
             const index = this.courses.findIndex(course => course.id === id)
             this.courses[index] = {
                 id,
@@ -36,10 +40,10 @@ export class CoursesService {
         }
     }
 
-   remove(id:number) {
-    const index = this.courses.findIndex(course => course.id === id)
-    if(index >= 0) {
-        this.courses.splice(index, 1)
+    remove(id: number) {
+        const index = this.courses.findIndex(course => course.id === id)
+        if (index >= 0) {
+            this.courses.splice(index, 1)
+        }
     }
-   }
 }
