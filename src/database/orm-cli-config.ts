2b23b9149a763f275524e284e4c061e40cf8768c
Rafael Pardinho <rafael.pardinho@rocketclouds.com.br>
2023-12-20 20:48:17 -0300

    feat: adicionado migrations AddCoursesTagsTable

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -3,6 +3,7 @@ import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1703071055411 } from 'src/migrations/1703071055411-CreateCoursesTable';
 import { CreateTagsTable1703081863814 } from 'src/migrations/1703081863814-CreateTagsTable';
 import { CreateCoursesTagsTable1703099843640 } from 'src/migrations/1703099843640-CreateCoursesTagsTable';
+import { AddCoursesTagsTable1703115274387 } from 'src/migrations/1703115274387-AddCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
@@ -11,5 +12,6 @@ export const dataSource = new DataSource({
     CreateCoursesTable1703071055411,
     CreateTagsTable1703081863814,
     CreateCoursesTagsTable1703099843640,
+    AddCoursesTagsTable1703115274387,
   ],
 });
