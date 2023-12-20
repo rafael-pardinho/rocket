25c3e760778f30c8c04f6de94a35f3c4fa092cb8
Rafael Pardinho <rafael.pardinho@rocketclouds.com.br>
2023-12-20 11:31:10 -0300

    feat: adicionado migrations tags e gerado a tabela tags

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1703071055411 } from 'src/migrations/1703071055411-CreateCoursesTable';
+import { CreateTagsTable1703081863814 } from 'src/migrations/1703081863814-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1703071055411],
+  migrations: [CreateCoursesTable1703071055411, CreateTagsTable1703081863814],
 });
