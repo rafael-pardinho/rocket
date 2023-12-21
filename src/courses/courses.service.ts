bbea106aadd0b5bd87d655fd7d835f0052d55916
Rafael Pardinho <rafael.pardinho@rocketclouds.com.br>
2023-12-21 12:07:06 -0300

    feat: refactory repository course e tags

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -8,12 +8,10 @@ import { CreateCourseDTO } from './dto/create-course-dto';
 
 @Injectable()
 export class CoursesService {
-  constructor(
-    @InjectRepository(Course)
-    private readonly courseRepository: Repository<Course>,
-    @InjectRepository(Tag)
-    private readonly tagRepository: Repository<Tag>,
-  ) {}
+  @InjectRepository(Course)
+  private readonly courseRepository: Repository<Course>;
+  @InjectRepository(Tag)
+  private readonly tagRepository: Repository<Tag>;
 
   async finAll() {
     return this.courseRepository.find({
