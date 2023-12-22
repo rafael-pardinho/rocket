d438b4c6b5ca123ac2fed15f12eb072161894875
Rafael Pardinho <rafael.pardinho@rocketclouds.com.br>
2023-12-22 07:43:01 -0300

    test: courses.service ajuste nos teste

--- src/courses/courses.service.spec.ts
+++ src/courses/courses.service.spec.ts
@@ -1,10 +1,50 @@
+import { randomUUID } from 'node:crypto';
 import { CoursesService } from './courses.service';
+import { AnyBulkWriteOperation } from 'typeorm';
 
-describe('CoursesService', () => {
+describe('CoursesService unit Tests', () => {
   let service: CoursesService;
+  let id: string;
+  let created_at: Date;
+  let expectOutputTags: any;
+  let expectOutputCourses: any;
+  let mockCourseRepository: any;
+  let mockTagRepository: any;
 
   beforeEach(async () => {
     service = new CoursesService();
+    id = randomUUID();
+    created_at = new Date();
+    expectOutputTags = [
+      {
+        id,
+        name: 'eco',
+        created_at,
+      },
+    ];
+    expectOutputCourses = {
+      id,
+      name: 'test',
+      description: 'test description',
+      created_at,
+      tags: expectOutputTags,
+    };
+
+    mockCourseRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      save: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      update: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      preload: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findAll: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      find: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+      remove: jest.fn().mockReturnValue(Promise.resolve(expectOutputCourses)),
+    };
+
+    mockTagRepository = {
+      create: jest.fn().mockReturnValue(Promise.resolve(expectOutputTags)),
+      findOne: jest.fn().mockReturnValue(Promise.resolve(expectOutputTags)),
+    };
   });
 
   it('should be defined', () => {
